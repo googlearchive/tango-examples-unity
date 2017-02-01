8e62af2641647c6440d2c97cf5046884efeec909
nick mccormick <hwerzog@gmail.com>
2017-02-01 15:50:03 -0800

    Add error message for 3D reconstruction in unity editor

--- UnityExamples/Assets/TangoSDK/Core/Scripts/TangoWrappers/Tango3DReconstruction.cs
+++ UnityExamples/Assets/TangoSDK/Core/Scripts/TangoWrappers/Tango3DReconstruction.cs
@@ -312,6 +312,15 @@ namespace Tango
                 return;
             }
 
+#if UNITY_EDITOR
+            if (m_context == IntPtr.Zero) 
+            {
+                Debug.LogError("Mesh Reconstruction is not currently supported in the Unity Editor.");
+                m_enabled = false;
+                return;
+            }
+#endif
+
             // Build World T depth camera
             TangoPoseData world_T_devicePose = new TangoPoseData();
             if (m_useAreaDescriptionPose)
@@ -356,6 +365,15 @@ namespace Tango
             {
                 return;
             }
+
+#if UNITY_EDITOR
+            if (m_context == IntPtr.Zero) 
+            {
+                Debug.LogError("Mesh Reconstruction is not currently supported in the Unity Editor.");
+                m_enabled = false;
+                return;
+            }
+#endif
 
             if (cameraId != TangoEnums.TangoCameraId.TANGO_CAMERA_COLOR)
             {
